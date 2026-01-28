da97d3bf086761f4502e073d56fb3a1689e1a606
yappy <superunyo@yahoo.co.jp>
2026-01-28 16:53:14 +0900

    Add `git gc` to allclean.py

--- tools/allclean.py
+++ tools/allclean.py
@@ -5,35 +5,39 @@ import pathlib
 import subprocess
 
 CLEAN_PATTERN = [
-	("make", "**/Makefile", ["make", "clean"]),
-	("cargo", "**/Cargo.toml", ["cargo", "clean"]),
+    ("git", "**/.git", ["git", "gc"]),
+    ("make", "**/Makefile", ["make", "clean"]),
+    ("cargo", "**/Cargo.toml", ["cargo", "clean"]),
 ]
 
+
 def clean_all(root, pat, cmd):
-	for p in pathlib.Path(root).glob(pat):
-		dir = p.parent
-		# Ignore if under hidden directory (**/.*/**)
-		if all((not p.name.startswith(".") for p in dir.parents)):
-			print(" ".join(cmd))
-			print(f"in {dir}")
-			subprocess.run(cmd, cwd=dir, check=False)
-			print()
+    for p in pathlib.Path(root).glob(pat):
+        dir = p.parent
+        # Ignore if under hidden directory (**/.*/**)
+        if all((not p.name.startswith(".") for p in dir.parents)):
+            print(" ".join(cmd))
+            print(f"in {dir}")
+            subprocess.run(cmd, cwd=dir, check=False)
+            print()
+
 
 def main():
-	parser = argparse.ArgumentParser(description="Find build file and clean")
-	for tag, pat, cmd in CLEAN_PATTERN:
-		parser.add_argument(f"--{tag}", action="store_true", help=f"Find {pat} and `{' '.join(cmd)}`")
-	parser.add_argument("root", help="root dir")
-	args = vars(parser.parse_args())
+    parser = argparse.ArgumentParser(description="Find build file and clean")
+    for tag, pat, cmd in CLEAN_PATTERN:
+        parser.add_argument(f"--{tag}", action="store_true", help=f"Find {pat} and `{' '.join(cmd)}`")
+    parser.add_argument("root", help="root dir")
+    args = vars(parser.parse_args())
 
-	exec_any = False
-	for tag, pat, cmd in CLEAN_PATTERN:
-		if args[tag]:
-			exec_any = True
-			clean_all(args["root"], pat, cmd)
-	if not exec_any:
-		print("Specify at least one clean option.")
-		print("-h or --help to show help.")
+    exec_any = False
+    for tag, pat, cmd in CLEAN_PATTERN:
+        if args[tag]:
+            exec_any = True
+            clean_all(args["root"], pat, cmd)
+    if not exec_any:
+        print("Specify at least one clean option.")
+        print("-h or --help to show help.")
+
 
 if __name__ == "__main__":
-	main()
+    main()
